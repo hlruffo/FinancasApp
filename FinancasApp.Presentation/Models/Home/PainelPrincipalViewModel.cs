f3d528454b71c99053f5cfc8e32559df9dab2f3c
hlruffo <hlruffo@gmail.com>
2023-12-07 21:18:32 -0300

    Cria model Painel principal

---
+++ FinancasApp.Presentation/Models/Home/PainelPrincipalViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace FinancasApp.Presentation.Models.Home
+{
+    /// <summary>
+    /// modelo de dados para a pagina principal do nosso projeto
+    /// </summary>
+    public class PainelPrincipalViewModel
+    {
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+        public decimal? TotalReceitas { get; set; }
+        public decimal? TotalDespesas { get; set; }
+        public decimal? Saldo { get; set; }
+        public decimal? Situacao { get; set; }
+        public List<ChartViewModel>? GraficoDonut { get; set; }
+        public List<ChartViewModel>? GraficoColunas { get; set; }
+
+    }
+    public class ChartViewModel
+    {
+        public string? Nome { get; set; }
+        public decimal? Valor { get;set; }
+    }
+
+}
