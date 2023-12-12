454e6e98d5faabf661a4afad018765130da57477
hlruffo <hlruffo@gmail.com>
2023-12-12 18:26:53 -0300

    ajuste da pagina inicia

--- FinancasApp.Presentation/Controllers/HomeController.cs
+++ FinancasApp.Presentation/Controllers/HomeController.cs
@@ -5,10 +5,10 @@ using FinancasApp.Presentation.Models.Home;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Data;
 
 namespace FinancasApp.Presentation.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IMovimentacaoDomainService? _movimentacaoDomainService;
@@ -18,46 +18,42 @@ namespace FinancasApp.Presentation.Controllers
             _movimentacaoDomainService = movimentacaoDomainService;
         }
 
-
         /// <summary>
-        /// Abrir pagina /Home/Index
+        /// Método para abrir a página /Home/Index
         /// </summary>
-        /// <returns></returns>
-        [Authorize]
         public IActionResult Index()
         {
             var model = new PainelPrincipalViewModel();
+
             try
             {
                 var dataAtual = DateTime.Now;
                 model.DataInicio = new DateTime(dataAtual.Year, dataAtual.Month, 1);
                 model.DataFim = model.DataInicio?.AddMonths(1).AddDays(-1);
 
-                //consultar movimentações do usuario autenticado no mes atual
+                //consultar as movimentações do usuário autenticado no mês atual
                 var usuario = JsonConvert.DeserializeObject<UserAuthViewModel>(User.Identity.Name);
-                var movimentacoes = _movimentacaoDomainService
+                var movimentacoes = _movimentacaoDomainService?
                     .Consultar(model.DataInicio.Value, model.DataFim.Value, usuario.Id.Value);
 
-                //guardar das informações para exibir
-                model.TotalReceitas = movimentacoes
-                    .Where(m => m.Tipo == TipoMovimentacao.Receita)
-                    .Sum(m => m.Valor);
-
-                model.TotalDespesas = movimentacoes
-                    .Where(m => m.Tipo == TipoMovimentacao.Despesa)
-                    .Sum(m => m.Valor);
-                
+                //guardando as informações para exibir na página
+                model.TotalReceitas = movimentacoes.Where(m => m.Tipo == TipoMovimentacao.Receita).Sum(m => m.Valor);
+                model.TotalDespesas = movimentacoes.Where(m => m.Tipo == TipoMovimentacao.Despesa).Sum(m => m.Valor);
                 model.Saldo = model.TotalReceitas - model.TotalDespesas;
-
                 model.Situacao = model.Saldo > 0 ? "Saldo Positivo" : model.Saldo < 0 ? "Saldo devedor" : "Saldo nulo";
-
             }
             catch (Exception e)
             {
                 TempData["MensagemErro"] = e.Message;
             }
-            
-            return View();
+
+            return View(model);
         }
     }
 }
+
+
+
+
+
+
